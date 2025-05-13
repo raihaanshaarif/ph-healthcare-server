2617b61ad5bec3ed9eceb2a0ea24b9d0dd65fa4f
raihaanshaarif <codexbd@gmail.com>
2025-05-13 19:24:15 +0600

    Add 404 error handling middleware to catch invalid routes and show error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application,  Request, Response } from "express";
+import express, { Application,  NextFunction,  Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import httpStatus from "http-status";
+
 
 
 const app: Application = express();
@@ -16,5 +18,16 @@ app.post("/", (req: Request, res: Response) => {
 
 app.use("/api/v1", router);
 app.use(globalErrorHandler);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    error:{
+      path: req.originalUrl,
+      message: "Path Not Found",
+    }
+})
+  next();
+});
 
 export default app;
