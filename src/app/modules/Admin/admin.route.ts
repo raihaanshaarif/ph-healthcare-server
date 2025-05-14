658803d74ebd1c1ae92f57ae6627b2c3c9a5e479
raihaanshaarif <codexbd@gmail.com>
2025-05-14 17:19:25 +0600

    Add authentication middleware to admin routes for role-based access control  29.4

--- src/app/modules/Admin/admin.route.ts
+++ src/app/modules/Admin/admin.route.ts
@@ -3,16 +3,28 @@ import { AdminController } from "./admin.controller";
 
 import validateRequest from "../../middlewares/validateRequest";
 import { adminValidation } from "./admin.validation";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
-router.get("/", AdminController.getAllAdmins);
-router.get("/:id", AdminController.getSingleAdmin);
+router.get(
+  "/", 
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), 
+  AdminController.getAllAdmins);
+router.get(
+  "/:id", 
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getSingleAdmin);
 router.patch(
   "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(adminValidation.update),
   AdminController.updateSingleAdmin
 );
-router.delete("/:id", AdminController.deleteSingleAdmin);
+router.delete(
+  "/:id", 
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.deleteSingleAdmin);
 
 export const AdminRoutes = router;
